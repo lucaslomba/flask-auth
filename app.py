f573ab62509c70e4cfd3992cf4aad8809387e5ed
lucaslomba <lucasazevedolomba@gmail.com>
2024-04-05 08:27:14 -0300

    Add logout route

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user, logout_user, login_required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "Your secret key"
@@ -10,6 +10,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 login_manager = LoginManager()
 db.init_app(app)
 login_manager.init_app(app)
+
+# set login route
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
 @app.route("/login", methods=["POST"])
 def login():
@@ -21,9 +28,16 @@ def login():
         user = User.query.filter_by(username=username).first()
 
         if user and user.password == password:
+            login_user(user)
             return jsonify({"message": "Autenticação realizada com sucesso"})
 
     return jsonify({"message": "Credenciais inválidas"}), 400
+
+@app.route("/logout", methods=["GET"])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Logout realizada com sucesso"})
 
 if __name__ == '__main__':
     app.run(debug=True)
