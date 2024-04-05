f235309bc369e1b40cbdf37d286881eadec234d9
lucaslomba <lucasazevedolomba@gmail.com>
2024-04-05 08:08:33 -0300

    Add login route and remove example route

--- app.py
+++ app.py
@@ -1,16 +1,29 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from models.user import User
 from database import db
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "Your secret key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
+login_manager = LoginManager()
 db.init_app(app)
+login_manager.init_app(app)
 
-@app.route("/hello-world", methods=["GET"])
-def hello_world():
-    return "Hello"
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.password == password:
+            return jsonify({"message": "Autenticação realizada com sucesso"})
+
+    return jsonify({"message": "Credenciais inválidas"}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
